b49cee02f9eb8e8ce2b4ac14612670b3630b93f8
Jez Higgins <jez@jezuk.co.uk>
2021-06-21 22:24:46 +0100

    added parse command to dump out AST
    needs prettying up - colour codes maybe?

--- bin/cell.js
+++ bin/cell.js
@@ -11,9 +11,11 @@ import { program } from 'commander/esm.mjs'
 program
   .command('lex <sources...>')
   .description('Display the tokenized program source')
-  .action(sources => {
-    lexFiles(sources)
-  })
+  .action(lexFiles)
+program
+  .command('parse <sources...>')
+  .description('Display the program\'s AST')
+  .action(parseFiles)
 program
   .arguments('[sources...]')
   .action(sources => {
@@ -31,6 +33,63 @@ function lexFiles(files) {
     for (const token of lex(code))
       console.log(token)
   }
+}
+
+function parseFiles(files) {
+  for (const file of files) {
+    const code = readFileSync(file).toString()
+    for (const ast of parse(lex(code)))
+      prettyPrintAst(ast)
+  }
+}
+
+function prettyPrintAst(node, indent = 0) {
+  const print = line => console.log(pad(indent), line)
+  const [type, ...args] = node
+  switch (type) {
+    case 'symbol':
+    case 'number':
+      print(`[ ${type}, ${args[0]} ]`)
+      break;
+    case 'string':
+      print(`[ ${type}, '${args[0]}' ]`)
+      break;
+    case 'assignment':
+      print(`[ ${type}`)
+      prettyPrintAst(args[0], indent + 2)
+      prettyPrintAst(args[1], indent + 2)
+      print(']')
+      break;
+    case 'call':
+      print(`[ ${type}`)
+      prettyPrintAst(args[0], indent + 2)
+      print('  [')
+      for (const n of args[1])
+        prettyPrintAst(n, indent + 4)
+      print('  ]')
+      print(']')
+      break;
+    case 'function':
+      print(`[ ${type}`)
+      if (args[0].length) {
+        print('  [')
+        for (const n of m)
+          prettyPrintAst(n, indent + 4)
+        print('  ]')
+      } else {
+        print('  [ ]')
+      }
+      print('  [')
+      for (const n of args[1])
+        prettyPrintAst(n, indent + 4)
+      print('  ]')
+      print(']')
+      break;
+  }
+}
+
+function pad(indent) {
+  return "                                             ".substr(0, indent)
 }
 
 function runFiles(files) {
