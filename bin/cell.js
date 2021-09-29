61c1d4c4a428ba871e702b277d35467cc8047118
Jez Higgins <jez@jezuk.co.uk>
2021-09-29 13:29:57 +0100

    rework command line driver to lex and parse can accept keyboard input

--- bin/cell.js
+++ bin/cell.js
@@ -10,38 +10,40 @@ import { program } from 'commander/esm.mjs'
 import chalk from 'chalk'
 
 program
-  .command('lex <sources...>')
+  .command('lex [sources...]')
   .description('Display the tokenized program source')
-  .action(lexFiles)
+  .action(sources => run(sources, lexCode))
 program
-  .command('parse <sources...>')
+  .command('parse [sources...]')
   .description('Display the program\'s AST')
-  .action(parseFiles)
+  .action(sources => run(sources, parseCode))
 program
   .arguments('[sources...]')
-  .action(sources => {
-    if (sources.length === 0)
-      runRepl()
-    else
-      runFiles(sources)
-  })
+  .action(sources => run(sources, makeEvaluator()))
 
-program.parse(process.arv)
-
-function lexFiles(files) {
-  for (const file of files) {
-    const code = readFileSync(file).toString()
-    for (const token of lex(code))
-      console.log(`[ ${chalk.yellow(token[0])}, '${chalk.green(token[1])}' ]`)
-  }
+try {
+  program.parse(process.arv)
+} catch(e) {
+  console.error(e)
 }
 
-function parseFiles(files) {
-  for (const file of files) {
-    const code = readFileSync(file).toString()
-    for (const ast of parse(lex(code)))
-      prettyPrintAst(ast)
-  }
+function lexCode(code) {
+  for (const token of lex(code))
+    console.log(` [ ${chalk.yellow(token[0])}, '${chalk.green(token[1])}' ]`)
+}
+
+function parseCode(code) {
+  for (const ast of parse(lex(code)))
+    prettyPrintAst(ast)
+}
+
+function evaluateCode(code, env) {
+  evaluate(parse(lex(code)), env)
+}
+
+function makeEvaluator() {
+  const env = topLevelEnvironment();
+  return code => evaluateCode(code, env);
 }
 
 function prettyPrintAst(node, indent = 0) {
@@ -105,21 +107,27 @@ function pad(indent) {
   return "                                             ".substr(0, indent)
 }
 
-function runFiles(files) {
-  const env = topLevelEnvironment()
+function run(sources, runFn) {
+  if (sources.length === 0)
+    runRepl(runFn)
+  else
+    runFiles(sources, runFn)
+}
+
+function runFiles(files, runFn) {
   for (const file of files) {
     if (file === '-')
-      runRepl(env)
+      runRepl(runFn)
     else {
       const code = readFileSync(file).toString()
-      evaluate(parse(lex(code)), env)
+      runFn(code)
     }
   }
 }
 
-function runRepl(env = topLevelEnvironment()) {
+function runRepl(runFn) {
   const evalCell = (cmd, context, filename, callback) => {
-    callback(null, evaluate(parse(lex(cmd)), env))
+    callback(null, runFn(cmd))
   }
 
   repl.start({ prompt: '>>> ', eval: evalCell });
